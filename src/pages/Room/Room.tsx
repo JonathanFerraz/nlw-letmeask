3f49bfb00c3efff2f3ceab630970721a589ad42e
Jonathan Ferraz <jonathan.chagas@e-unicred.com.br>
2021-06-24 13:19:25 -0300

    feat(roompage): added page room

---
+++ src/pages/Room/Room.tsx
@@ -0,0 +1,161 @@
+import React, { FormEvent, useEffect, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import { useParams } from 'react-router-dom';
+
+import logoImg from '../../assets/icons/logo.svg';
+import { Button, Img, RoomCode } from '../../components';
+import { useAuth } from '../../hooks/useAuth';
+import { database } from '../../services/firebase';
+import { PageRoom, Main, RoomTitle } from './styles';
+
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+  }
+>;
+
+type Question = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+};
+
+type RoomParams = {
+  id: string;
+};
+
+export const Room: React.FC = () => {
+  const [newQuestion, setNewQuestion] = useState<string>('');
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [title, setTitle] = useState();
+  const { user } = useAuth();
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  const notify = () => toast.success('Pergunta cadastrada com sucesso.');
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHighlighted,
+            isAnswered: value.isAnswered,
+          };
+        },
+      );
+
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    });
+  }, [roomId]);
+
+  const delay = (amount = 750) =>
+    new Promise(resolve => {
+      setIsLoading(true);
+      setTimeout(resolve, amount);
+    });
+
+  const handleCreateNewQuestion = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (newQuestion.trim() === '') {
+      return;
+    }
+
+    if (!user) {
+      throw new Error('You must be logged in');
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false,
+    };
+
+    await delay(750);
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+    setIsLoading(false);
+    notify();
+    setNewQuestion('');
+  };
+
+  return (
+    <PageRoom>
+      <Toaster />
+      <header>
+        <div>
+          <Img src={logoImg} alt={'Logotipo da aplicação'} />
+          <RoomCode code={roomId} />
+        </div>
+      </header>
+
+      <Main>
+        <RoomTitle>
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && (
+            <span>
+              {questions.length}{' '}
+              {questions.length >= 2 ? 'perguntas' : 'pergunta'}
+            </span>
+          )}
+        </RoomTitle>
+        <form onSubmit={handleCreateNewQuestion}>
+          <textarea
+            placeholder={'O que você quer perguntar?'}
+            onChange={event => setNewQuestion(event.target.value)}
+            value={newQuestion}
+          />
+
+          <div>
+            {user ? (
+              <div className="user-info">
+                <Img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>
+                Para enviar uma pergunta, <button>faça seu login</button>.
+              </span>
+            )}
+            <Button
+              variant={'solid'}
+              type={'submit'}
+              style={{ padding: '0 3rem' }}
+              disabled={!user}
+              loading={isLoading}
+              loadingMessage={'Enviando pergunta...'}
+            >
+              Enviar Pergunta
+            </Button>
+          </div>
+        </form>
+      </Main>
+    </PageRoom>
+  );
+};
---
+++ src/pages/Room/styles.ts
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+
+export const PageRoom = styled.div`
+  header {
+    padding: 24px;
+    border-bottom: 1px solid #e2e2e2;
+
+    > div {
+      max-width: 1120px;
+      margin: 0 auto;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      > img {
+        max-height: 45px;
+      }
+    }
+  }
+`;
+
+export const Main = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+
+  form {
+    textarea {
+      width: 100%;
+      border-radius: 8px;
+      padding: 1rem;
+      background: #fefefe;
+      box-shadow: 0 2px 12px rgba(0, 0, 0, 0.04);
+      resize: vertical;
+      min-height: 130px;
+    }
+
+    > div {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-top: 1rem;
+
+      .user-info {
+        display: flex;
+        align-items: center;
+
+        img {
+          width: 32px;
+          height: 32px;
+          border-radius: 50%;
+        }
+
+        span {
+          margin-left: 8px;
+          color: ${props => props.theme.textColor};
+          font-weight: 500;
+          font-size: 14px;
+        }
+      }
+
+      > span {
+        font-size: 14px;
+        color: #737380;
+        font-weight: 500;
+
+        button {
+          background: transparent;
+          font-size: 14px;
+          font-weight: 500;
+          color: ${props => props.theme.primaryColor};
+          text-decoration: underline;
+        }
+      }
+    }
+  }
+`;
+
+export const RoomTitle = styled.div`
+  margin: 32px 0 24px;
+  display: flex;
+  align-items: center;
+
+  h1 {
+    font-family: 'Poppins', sans-serif;
+    font-size: 24px;
+    color: ${props => props.theme.textColor};
+  }
+
+  span {
+    margin-left: 1rem;
+    background: #e559f9;
+    border-radius: 50px;
+    padding: 0.5rem 1rem;
+    color: ${props => props.theme.textWhite};
+    font-weight: 500;
+    font-size: 14px;
+  }
+`;
